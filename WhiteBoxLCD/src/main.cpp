59d9ac27d1053c80d9d125a6b7b10b7ec7393e73
Jan Kjetil Wik <jankwik@gmail.com>
2022-07-25 23:01:43 +0200

    Working LCD display

--- WhiteBoxLCD/src/main.cpp
+++ WhiteBoxLCD/src/main.cpp
@@ -3,11 +3,16 @@
 
 TM1651* tm1651;
 
+#define CLOCK_PIN   3
+#define DATA_PIN    5
+
 void setup() {
-  tm1651 = new TM1651(3, 2);
+  tm1651 = new TM1651(CLOCK_PIN, DATA_PIN);
+  tm1651->displayClear();
+  tm1651->displaySet();
   tm1651->displayInteger(133);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(500);
 }
